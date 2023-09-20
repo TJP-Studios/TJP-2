022e5fa44e57c364ed4c662d12d66614b78e6fcf
Bashua Mutiat <mutiatbashua@gmail.com>
2023-09-20 09:44:37 +0100

    some changes to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
     <div className="p-4 sm:p-10 flex flex-col sm:flex-row">
       <div className=" flex flex-col gap-3">
         <h1 className="font-bold text-[3.5rem] leading-[4.105625rem]">
-          <span>We</span> Listen, <span>We</span> Advice, <span>We</span>
+          <span>We</span> Listen, <span>We</span> Advice, <span>We </span>
           Defend.
         </h1>
         <p>
